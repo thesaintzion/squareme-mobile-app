fa3c4211a238528c15d1d4c40f690e6fb14eb44e
St. Stephen <saintzion777@gmail.com>
2023-12-06 14:29:27 +0100

    Resend otp countdown timer

--- src/screens/PhoneOtpScreen/PhoneOtpScreen.tsx
+++ src/screens/PhoneOtpScreen/PhoneOtpScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Alert, ScrollView, StatusBar, StyleSheet, View } from "react-native";
 
 import { AppButton, NavHeader, Spacer, AppText, OtpInput, SuccessModal } from "@src/components";
@@ -9,6 +9,7 @@ import { colors, routes } from "@src/utils";
 interface LocalProps extends ScreenProps { }
 
 export const PhoneOtpScreen: React.FC<LocalProps> = ({ navigation }) => {
+    const [countdown, setCountdown] = useState(60);
     const [otp, setOtp] = useState<string[]>([]);
     const [isSuccessful, setIsSuccessful] = useState(false);
 
@@ -17,11 +18,40 @@ export const PhoneOtpScreen: React.FC<LocalProps> = ({ navigation }) => {
         setOtp(data);
     }
 
+    // We could add logic for resending OTP here...
+    // Like to make an API call to request a new OTP...
+    // Then reset the countdown to 60 seconds
     const handleSendOTP = (type: 'sms' | 'call' | 'whatsapp') => {
+        if (countdown > 0) {
+            Alert.alert(`Please wait for ${formatCountdown()} to resend`);
+            return;
+        }
         Alert.alert(`Get the otp via: ${type}`);
+        setCountdown(60);
     }
 
-    React.useEffect(() => {
+    const formatCountdown = () => {
+        const minutes = Math.floor(countdown / 60);
+        const seconds = countdown % 60;
+        return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    };
+
+
+    useEffect(() => {
+        let timer: any;
+
+        if (countdown > 0) {
+            timer = setInterval(() => {
+                setCountdown(prevCountdown => prevCountdown - 1);
+            }, 1000);
+        }
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, [countdown]);
+
+    useEffect(() => {
         if (otp.length === 5) {
             setIsSuccessful(true);
         } else {
@@ -44,19 +74,20 @@ export const PhoneOtpScreen: React.FC<LocalProps> = ({ navigation }) => {
 
                 <Spacer size={20} />
                 <AppText.Bold style={{ width: '80%', alignSelf: 'center', fontSize: 15, textAlign: 'center', color: colors.TEXT_PRIMARY }}>
-                    0.00
+                    {formatCountdown()}
+                    {/* 0.00 */}
                 </AppText.Bold>
                 <Spacer size={40} />
 
                 <AppText style={{ width: '80%', alignSelf: 'center', fontSize: 14, textAlign: 'center', color: '#4F4F4F' }}>
-                    Having trouble receiving SMS? <AppText style={{ color: colors.TEXT_PRIMARY, lineHeight: 0 }} onPress={() => handleSendOTP('sms')}>Resend</AppText>{'\n'}Or try other options below
+                    Having trouble receiving SMS? <AppText style={{ color: colors.TEXT_PRIMARY, lineHeight: 0 }} onPress={() => countdown == 0 ? handleSendOTP('sms') : null}>Resend</AppText>{'\n'}Or try other options below
                 </AppText>
             </ScrollView>
 
             {/* Disable buttons when timer still counting down */}
             <View style={styles.actionsCon}>
-                <AppButton onPress={() => handleSendOTP('whatsapp')} text='Call me' type='light' style={{ borderWidth: 1 }} />
-                <AppButton onPress={() => handleSendOTP('whatsapp')} text='Whatsapp' />
+                <AppButton disabled={countdown > 0} onPress={() => handleSendOTP('whatsapp')} text='Call me' type='light' style={{ borderWidth: 1 }} />
+                <AppButton disabled={countdown > 0} onPress={() => handleSendOTP('whatsapp')} text='Whatsapp' />
             </View>
 
             {isSuccessful && <SuccessModal
